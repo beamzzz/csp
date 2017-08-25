8b14163a754a5e8a4a479311273a11db31aa1a10
beamzzz <1229zcl>
2017-08-25 18:27:34 +0800

    修改SESSION

--- src/main/java/com/mx/web/CsmController.java
+++ src/main/java/com/mx/web/CsmController.java
@@ -1,8 +1,14 @@
 package com.mx.web;
 
+import com.mx.domain.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * @aother zcl
@@ -12,8 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/csp")
 public class CsmController {
 
+    Logger logger = LoggerFactory.getLogger(CsmController.class);
+
     @GetMapping("/getCsp")
     public String getCsp(){
         return "success";
+    }
+
+    @GetMapping("/getSession")
+    public String getSession(HttpServletRequest request){
+
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        logger.info(session.getId());
+        logger.info(user.getUserCode());
+        return  session.getId();
     }
 }
